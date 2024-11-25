78cdddb3a3797f21cadf8c37c64668d29e580fbe
pool2win <pool2win@users.noreply.github.com>
2024-11-25 08:36:06 +0100

    First pass at making DKG run end to end

--- src/node/protocol/dkg/trigger.rs
+++ src/node/protocol/dkg/trigger.rs
@@ -23,6 +23,7 @@ use crate::node::protocol::{dkg, Protocol};
 #[mockall_double::double]
 use crate::node::reliable_sender::ReliableSenderHandle;
 use crate::node::State;
+use frost_secp256k1 as frost;
 use tokio::time::{Duration, Instant};
 use tower::ServiceExt;
 
@@ -64,7 +65,7 @@ pub(crate) async fn trigger_dkg_round_one(
     let echo_broadcast_service = EchoBroadcast::new(
         protocol_service.clone(),
         echo_broadcast_handle,
-        state,
+        state.clone(),
         node_id.clone(),
     );
 
@@ -73,22 +74,68 @@ pub(crate) async fn trigger_dkg_round_one(
         .timeout(Duration::from_secs(10))
         .service(echo_broadcast_service);
 
-    let _ = echo_broadcast_timeout_service
-        .oneshot(dkg::round_one::PackageMessage::new(node_id.clone(), None).into())
-        .await;
-    log::info!("DKG Echo broadcast finished");
-
-    // TODO[DKG]: We need to check if round one successfully finised.
+    let round1_future = echo_broadcast_timeout_service
+        .oneshot(dkg::round_one::PackageMessage::new(node_id.clone(), None).into());
+    // log::info!("DKG Echo broadcast finished");
 
     log::info!("Sending DKG round two message");
     let round_two_timeout_service = tower::ServiceBuilder::new()
         .timeout(Duration::from_secs(10))
         .service(protocol_service);
 
-    let _ = round_two_timeout_service
-        .oneshot(dkg::round_two::PackageMessage::new(node_id, None).into())
-        .await;
+    let round2_future = round_two_timeout_service
+        .oneshot(dkg::round_two::PackageMessage::new(node_id, None).into());
     log::info!("DKG round two message finished");
+    let sleep_future = tokio::time::sleep(Duration::from_secs(5));
+
+    // TODO Improve this to allow round1 to finish as soon as all other parties have sent their round1 message
+    // This will mean moving the timeout into round1 service
+
+    // Wait for round1 to finish, givt it 5 seconds
+    let (_, round1_result) = tokio::join!(sleep_future, round1_future);
+    if round1_result.is_err() {
+        log::error!("Error running round1 {:?}", round1_result.err());
+        return;
+    }
+    log::info!("Round 1 finished");
+
+    // start round2
+    let round2_result = round2_future.await;
+    if round2_result.is_err() {
+        log::error!("Error running round2 {:?}", round2_result.err());
+        return;
+    }
+    log::info!("Round 2 finished");
+
+    let round2_secret_package = state
+        .dkg_state
+        .get_round2_secret_package()
+        .await
+        .unwrap()
+        .unwrap();
+    let round1_packages = state
+        .dkg_state
+        .get_received_round1_packages()
+        .await
+        .unwrap();
+    let round2_packages = state
+        .dkg_state
+        .get_received_round2_packages()
+        .await
+        .unwrap();
+    let dkg_result =
+        frost::keys::dkg::part3(&round2_secret_package, &round1_packages, &round2_packages);
+
+    if dkg_result.is_err() {
+        log::error!("Error running DKG part3 {:?}", dkg_result.err());
+        return;
+    }
+    let (key_package, pubkey_package) = dkg_result.unwrap();
+    log::info!(
+        "DKG part3 finished. key_package = {:?}, pubkey_package = {:?}",
+        key_package,
+        pubkey_package
+    );
 }
 
 #[cfg(test)]
@@ -141,7 +188,7 @@ mod dkg_trigger_tests {
 
         // Wait for just over one interval to ensure we get at least one trigger
         let result: Result<(), time::error::Elapsed> = timeout(
-            Duration::from_millis(110),
+            Duration::from_millis(10),
             run_dkg_trigger(
                 15,
                 node_id,
